e57d001ecde2e0afd969fba346cfcadaf3d3034f
Niels Aan de Brugh <nielsadb@gmail.com>
2022-07-09 22:20:54 +0200

    Tag matching support

--- idx.py
+++ idx.py
@@ -17,12 +17,14 @@ from rich import print
 
 app = typer.Typer()
 
+
 @dataclass
 class Config:
     root: Optional[Path]
     cache_file: Path
     cache: dict[Path, Node]
     verbose: bool
+
     def tops(self):
         if self.root:
             if self.root in self.cache:
@@ -32,28 +34,38 @@ class Config:
         else:
             yield from self.cache.values()
 
+
 def _make_cfg_singleton():
     _cfg: list[Config] = [None]
-    def set_cfg(cfg:Config):
+
+    def set_cfg(cfg: Config):
         _cfg[0] = cfg
+
     def get_cfg() -> Config:
         return _cfg[0]
     return set_cfg, get_cfg
-set_sfg, get_cfg = _make_cfg_singleton()    
 
-def out_msg(str:str, verbose:bool = None):
+
+set_sfg, get_cfg = _make_cfg_singleton()
+
+
+def out_msg(str: str, verbose: bool = None):
     if verbose == None:
         verbose = get_cfg().verbose
     if verbose:
         print(str)
 
-def fatal(str:str):
+
+def fatal(str: str):
     out_msg(str, True)
     exit(1)
 
+
 DEFAULT_CACHE_FILE = Path.home()/".idx_cache.pickle"
+
+
 @app.callback()
-def load(root:Optional[Path] = None, cache_file:Path = DEFAULT_CACHE_FILE, verbose:bool = False):
+def load(root: Optional[Path] = None, cache_file: Path = DEFAULT_CACHE_FILE, verbose: bool = False):
     if cache_file.exists():
         out_msg(f"loading cache from {cache_file}.", verbose)
         with open(cache_file, "rb") as fd:
@@ -65,16 +77,12 @@ def load(root:Optional[Path] = None, cache_file:Path = DEFAULT_CACHE_FILE, verbo
         out_msg(f"{root} not found in cache.", verbose)
     set_sfg(Config(root, cache_file, cache, verbose))
 
+
 def write_cache():
     out_msg(f"storing cache in {get_cfg().cache_file}.")
     with open(get_cfg().cache_file, "wb") as fd:
         pickle.dump(get_cfg().cache, fd)
 
-def tops():
-    if get_cfg().root:
-        yield get_cfg().cache[get_cfg]
-
-### Commands ###################################
 
 @app.command()
 def index():
@@ -86,45 +94,54 @@ def index():
     get_cfg().cache[root] = top
     write_cache()
 
+
 @app.command()
 def show():
-    def visit(node:Node, indent:int):
+    def visit(node: Node, indent: int):
         tags = ", ".join(node.tags)
         print(f"{indent*'  '}{node.path.name} {f'[{tags}]' if tags else ''}")
         return indent+1
     for top in get_cfg().tops():
         top.walk(visit, 0)
 
+
 @app.command()
 def cached():
     for root in get_cfg().cache.keys():
         print(f"* {root}")
 
+
 @app.command()
-def search(regexps:list[str], md:int = 1, ic:bool = True):
-    # Comile regexps for better performance.
-    crs = [re.compile(r, re.IGNORECASE if ic else 0) for r in regexps]
-    # Process a single tree.
-    def search_top(top:Node, results):
-        def visit(node:Node, depth):
+def search(regexps: list[str], md: int = 1, ic: bool = True):
+    name_cnd = []
+    tags_cnd = []
+    for r in regexps:
+        if r.startswith("@"):
+            tags_cnd.append(r[1:])
+        else:
+            name_cnd.append(re.compile(r, re.IGNORECASE if ic else 0))
+
+    def search_top(top: Node, results):
+        def visit(node: Node, depth):
             if not node.children and depth >= md:
                 term = "/".join(node.path.relative_to(top.path).parts[md:])
-                matches = list(r.search(term) for r in crs)
-                if all(matches):
-                    key = "".join(re.findall("\w", term.lower()))
-                    results.append((key, term, node))
+                if all(r.search(term) for r in name_cnd):
+                    lower_tags = set(tag.lower() for tag in node.tags)
+                    if all(tag in lower_tags for tag in tags_cnd):
+                        key = "".join(re.findall("\w", term.lower()))
+                        results.append((key, term, node))
             return depth+1
         top.walk(visit, 0)
-    # Process all trees.
+
     results = []
     for top in get_cfg().tops():
         search_top(top, results)
-    # Present results.
     print(f"[dim]{'-'*80}[/dim]")
     for (_, term, node) in sorted(results, key=operator.itemgetter(0)):
         link = f"file://{urllib.parse.quote(node.path.as_posix())}"
         print(f"[bold]*[/bold] [link={link}]{term}[/link]")
     print(f"[dim]{len(results)} results[/dim]")
+
 
 if __name__ == "__main__":
     app()
